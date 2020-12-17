55415a1a96ad70ca02ff2160a5d87542de218cc9
Pedro Bertao <pedrobertao11@hotmail.com>
2020-12-17 14:28:05 -0300

    add database pkg

--- main.go
+++ main.go
@@ -8,27 +8,44 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/joho/godotenv"
+	"github.com/pedrobertao/backend-hackathon-klever-2020/database"
+	"go.uber.org/zap"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("Port not set")
-	}
-
+func serve() {
 	router := gin.New()
-	router.Use(gin.Logger())
-
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "Klever ID is live",
 		})
 	})
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port not set")
+	}
 	router.Run(":" + port)
+}
+
+func main() {
+	zapConfig := zap.NewDevelopmentConfig()
+	logger, err := zapConfig.Build()
+	if err != nil {
+		log.Fatal("Error to init zap global logger")
+	}
+	zap.ReplaceGlobals(logger)
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	zap.L().Info("Env loaded")
+
+	if err := database.Connect(); err != nil {
+		log.Fatal("Error connecting to database")
+	}
+	defer database.Stop()
+	zap.L().Info("Database connected")
+
+	serve()
 }
